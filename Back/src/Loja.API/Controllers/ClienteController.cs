1fd903e1cf4638d9800f4405820184e86adeb767
Felipe Braghittoni <felipe.braghittoni1999@gmail.com>
2023-05-08 15:50:12 -0300

    first comit

---
+++ Back/src/Loja.API/Controllers/ClienteController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Loja.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Loja.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClienteController : ControllerBase
+    {
+       public IEnumerable<Cliente> _cliente = new Cliente[]{
+         new Cliente(){
+                ClienteId=1,
+                Nome="Felipe",
+                Sobrenome="Brag",
+                CPF="123.456.123-23",
+                Email="felipebrag@gmail.com",
+                Senha="felipe13",
+                ConfSenha="felipe13",
+                },
+                new Cliente(){
+                ClienteId=2,
+                Nome="Felipe",
+                Sobrenome="Brag",
+                CPF="123.456.123-23",
+                Email="felipebrag@gmail.com",
+                Senha="felipe13",
+                ConfSenha="felipe13",
+                }
+       };
+
+       public ClienteController(){
+
+       }
+
+        [HttpGet]
+        public IEnumerable<Cliente> Get()
+        {
+            return _cliente;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Cliente> GetById(int id)
+        {
+            return _cliente.Where(cliente => cliente.ClienteId == id);
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Exemplo do post";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"Exemplo de Put com id = {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"Exemplo de Delete com id = {id}";
+        }
+    }
+}
---
+++ Back/src/Loja.API/Models/Cliente.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Loja.API.Models
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string CPF { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string ConfSenha { get; set; }
+    }
+}
