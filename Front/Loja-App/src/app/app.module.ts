23bba52d0b6819963c7b7da514b163e3d0b82a48
Felipe Braghittoni <felipe.braghittoni1999@gmail.com>
2023-05-30 15:06:50 -0300

    update
    import do bootstrap

--- Front/Loja-App/src/app/app.module.ts
+++ Front/Loja-App/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { ClientesComponent } from './clientes/clientes.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
