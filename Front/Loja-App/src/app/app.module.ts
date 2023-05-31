0941756b7611020313bbf3f30ac42ac14659053e
Felipe Braghittoni <felipe.braghittoni1999@gmail.com>
2023-05-31 17:38:10 -0300

    update front
    tabela com clientes, função de pesquisa

--- Front/Loja-App/src/app/app.module.ts
+++ Front/Loja-App/src/app/app.module.ts
@@ -2,23 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClientesComponent } from './clientes/clientes.component';
+import { NavComponent } from './nav/nav.component';
+
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClientesComponent
-  ],
+    ClientesComponent,
+    NavComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
--- Front/Loja-App/src/app/clientes/clientes.component.ts
+++ Front/Loja-App/src/app/clientes/clientes.component.ts
@@ -7,7 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./clientes.component.scss']
 })
 export class ClientesComponent {
-  public clientes: any ;
+
+  public clientes: any =[];
+  public clientesFiltrados: any = [];
+  private _filtroLista:string = '';
+
+  public get filtroLista(): string{
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string){
+    this._filtroLista = value;
+    this.clientesFiltrados = this.filtroLista ? this.filtrarClientes(this.filtroLista) : this.clientes;
+  }
+
+  filtrarClientes(filtrarPor: string): any{
+    filtrarPor = filtrarPor.toLowerCase();
+    return this.clientes.filter(
+      (cliente: any) => cliente.nome.toLowerCase().indexOf(filtrarPor) !== -1 ||
+                        cliente.sobrenome.toLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
 
   constructor(private http: HttpClient){}
 
@@ -17,7 +37,10 @@ export class ClientesComponent {
 
   public getClientes():void{
     this.http.get('https://localhost:5001/api/Clientes').subscribe(
-      response => this.clientes = response,
+      response =>{
+        this.clientes = response;
+        this.clientesFiltrados = this.clientes
+      },
       error => console.log(error),
 
     );
